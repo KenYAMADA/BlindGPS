6456e835d059090e0129a92c113b6575db1ab34a
Ty Long <dtylerlong@gmail.com>
2012-03-18 13:03:27 -0400

    Project File Structure [Ty]

---
+++ src/com/codeguys/project/blindgps/BlindGPSActivity.java
@@ -0,0 +1,13 @@
+package com.codeguys.project.blindgps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlindGPSActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
